e5fb0ba060aa433493fb6c744852419a9f9675ea
Lucas Muto <lucasmuto@id.uff.br>
2025-03-28 17:24:56 -0300

    feat: implementa componente Header com logo

--- zaprecall/src/App.jsx
+++ zaprecall/src/App.jsx
@@ -1,14 +1,26 @@
-import GlobalStyle from './styles/GlobalStyle'
+import styled from 'styled-components';
+import GlobalStyle from './styles/GlobalStyle';
+import Header from './components/Header';
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 375px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 42px 24px;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>
-        ZapRecall
-      </div>
+      <Container>
+        <Header />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ zaprecall/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import logo from '../../assets/logo.png';
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 45px;
+  gap: 20px;
+`;
+
+const Logo = styled.img`
+  width: 52px;
+  height: 60px;
+`;
+
+const Title = styled.h1`
+  font-family: 'Recursive', sans-serif;
+  font-size: 36px;
+  color: #FFFFFF;
+  font-weight: 400;
+`;
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <Logo src={logo} alt="Logo ZapRecall" />
+      <Title>ZapRecall</Title>
+    </HeaderContainer>
+  );
+}
